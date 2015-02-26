6349e8eeb792d44c8a63f0649e983bc9bef0545c
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-02-26 11:14:34 -0800

    Improve voting and prediction converion scripts

--- convert_predictions.py
+++ convert_predictions.py
@@ -1,12 +1,12 @@
-"""
-
-.. moduleauthor:: Jan Van Bruggen <jancvanbruggen@gmail.com>
-"""
+from csv import reader
+from csv import writer
 
 
-def run():
-    pass
-
-
-if __name__ == '__main__':
-    run()
+with open('results/test.out') as rf_result_file:
+    rf_result_reader = reader(rf_result_file)
+    next(rf_result_reader)
+    with open('results/predictions_svm.txt', 'wb') as rf_predictions_file:
+        rf_predictions_writer = writer(rf_predictions_file)
+        rf_predictions_writer.writerow(['Id', 'Prediction'])
+        for row in rf_result_reader:
+            rf_predictions_writer.writerow([entry.strip('"') for entry in row[0].split()])
--- vote.py
+++ vote.py
@@ -1,12 +1,31 @@
-"""
-
-.. moduleauthor:: Jan Van Bruggen <jancvanbruggen@gmail.com>
-"""
+from csv import reader
+from csv import writer
 
 
-def run():
-    pass
+def read(filepath):
+    predictions = []
+    with open(filepath, 'rU') as datafile:
+        datareader = reader(datafile)
+        next(datareader)
+        for row in datareader:
+            predictions.append(row[1])
+    return predictions
 
+rf_tf_idf = read('results/predictions_rf_tf_idf.txt')
+rf_wc = read('results/predictions_rf_wc.txt')
+ada_wc = read('predictions_adaboost.txt')
 
-if __name__ == '__main__':
-    run()
+predictors = (rf_tf_idf, rf_wc, ada_wc)
+predictions = []
+for i in range(len(rf_tf_idf)):
+    vote = sum([int(predictor[i]) for predictor in predictors])
+    if vote >= 2:
+        predictions.append(1)
+    else:
+        predictions.append(0)
+
+with open('predictions_vote.txt', 'wb') as votefile:
+    votewriter = writer(votefile)
+    votewriter.writerow(['Id', 'Prediction'])
+    for i, prediction in enumerate(predictions):
+        votewriter.writerow([i+1, prediction])
